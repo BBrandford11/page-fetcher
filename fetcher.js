270b9bea8151ae2c10d2bbcaba2957982073cc9e
BBrandford11 <brandfordbryce@gmail.com>
2022-02-16 22:15:23 -0700

    fetcher

---
+++ fetcher.js
@@ -0,0 +1,26 @@
+const args = process.argv.slice(2);
+const fs = require('fs')
+const request = require('request');
+const site = args[0]
+const file = args[1]
+
+
+
+request(site, (error, response, body) => {
+  // console.log('error:', error); // Print the error if one occurred
+  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // console.log('body:', body); // Print the HTML for the Google homepage.
+  
+  const filesize = body.length
+
+  fs.writeFile(file, body, err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    //file written successfully
+    console.log(`Downloaded and saved ${filesize} bytes to ./index.html`)
+  })
+});
+
+
